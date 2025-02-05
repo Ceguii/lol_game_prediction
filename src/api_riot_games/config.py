899c44f623408e0acf7053605e9cc93cbf61a281
cyril.chen <chencyril482@gmail.com>
2025-02-05 16:06:45 +0100

    update - add db config

--- src/api_riot_games/config.py
+++ src/api_riot_games/config.py
@@ -4,12 +4,24 @@ from dotenv import load_dotenv
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
 
-# Récupérer les variables d'environnement
+# Récupérer les variables d'environnement sur lol
 API_KEY = os.getenv("API_KEY")
 REGION = os.getenv("REGION")
 RIOT_ID = os.getenv("RIOT_ID")
 TAGLINE = os.getenv("TAGLINE")
 
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD")
+}
+
 # Assurez-vous de vérifier si les variables sont chargées correctement
-if not all([API_KEY, REGION, RIOT_ID, TAGLINE]):
+if not all(
+    
+    [API_KEY, REGION, RIOT_ID, TAGLINE, DB_CONFIG]
+    
+    ):
+    
     raise ValueError("Certaines variables d'environnement manquent dans .env")
